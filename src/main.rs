ab46220ae60686d1d8949cb602ec3bbc703f25fd
Anwaarahmedshah <fa15be0011@maju.edu.pk>
2019-09-15 20:42:27 +0500

    Rust Programming Practice

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+fn main() {
+
+    /*
+    Assignment 01 first question!
+    let product_name = "multinational product";
+    let product_manufacturer = "AQBC limited karachi ";
+    let price = 2000;
+    println!("the product name is : {}  and the manufacturer is : {} and the price is : {}",product_name,product_manufacturer,price);
+*/
+
+/* mutation practice!!
+let mut tempe = 32;
+tempe = 33;
+println!("{}",tempe);
+*/
+
+/*
+const myn datatype batani hy like float64 f64
+const PI:f64 = 3.142;
+println!("{}",PI);
+*/
+
+/*  Assignment 01 second question!
+  
+let a = 4;
+let a = a + 5;
+
+println!("{}",a);
+
+let a = a * 4;
+println!("{}",a);
+*/
+
+/*  Assignment 01 3rd question!
+
+let floata:f64 = 3.2;
+let floatb:f64 = 3.0;
+
+println!("Addition is: {} ",floata + floatb);
+
+println!("Subtraction is: {} ",floata - floatb);
+
+
+println!("multiplication is: {} ",floata * floatb);
+
+
+println!("Division is: {} ",floata / floatb);
+
+*/
+/*
+printing the  ASCII CODE
+let c = b'A';
+println!("{}",c);
+*/
+
+/*  Assignment 01 4th question!
+let fruit = ("mango", 13.0 ,200);
+
+let(fruit_name,Weight,Price) = fruit;
+
+println!("fruit is {} weight is : {} , grams and its price is : {}",fruit_name,Weight,Price);
+*/
+let cars = ["passo","vitz","swift","tyotta","ferrari"];
+let prices =[800000,900000,950000,780000,1000000];
+
+println!("Car Name: {:#?} - Price : {:#?}",cars[0],prices[0]);
+println!("Car Name: {:#?} - Price : {:#?}",cars[1],prices[1]);
+println!("Car Name: {:#?} - Price : {:#?}",cars[2],prices[2]);
+
+}
