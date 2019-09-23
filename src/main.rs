aa998b9f9beb42757d474edb3713ca9d86165169
anwaar12345 <syedanwar016@gmail.com>
2019-09-23 20:55:03 +0500

    parameter wala function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Tell 1");
+    tell(1,2);
+
+}
+// function with parameter we have to give data type also
+fn tell(x: u64 , y: u64){
+
+     println!("value of x * x  is:{} and value of y*y is:{}",x*x,y*y);
+
 }
